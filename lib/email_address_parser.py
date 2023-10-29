77c8fc2ead1d4ab23e23ef318af4e60cba39b9c2
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-10-29 01:46:12 -0600

    this shows how to use regular expressions to determine if something is an email address and given a bunch of delimeters split a string into multiple strings

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,19 @@
-# your code goes here!
+import re;
+
+class EmailAddressParser:
+    def __init__(self, eadrssstr):
+        self.emailaddressstr = eadrssstr;
+
+    def parse(self):
+        regex = re.compile(r",\s|\s|,");
+        mlist = list(set(re.split(regex, self.emailaddressstr)));
+        retlist = [];
+        email_address = r"[A-z][A-z0-9._]*@[A-z]*\.[A-z]*";
+        email_regex = re.compile(email_address);
+        for item in mlist:
+            if (re.fullmatch(email_regex, item)): retlist.append(item);
+        return sorted(retlist);
+        #return list(set(self.emailaddressstr.split(", ")));
+
+parser = EmailAddressParser("john@doe.com, person@somewhere.org, who, what, where, when, why, how");
+print(parser.parse());
